6f1a9b2d8fc9010576866c8d73de7ff5f6f6f123
micro-hawk <dasvikas0071@gmail.com>
2021-04-27 10:27:17 +0530

    #2xbe67[IN PROGRESS] HERO SECTION PORTFOLIO

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ import "./Hero.css";
 export default function Hero() {
     return(
         <div>
-            <h1>Hello World</h1>
+            <h1>Hello, I am Vikas Das !</h1>
         </div>
     );
 }
