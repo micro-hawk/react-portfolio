d004fadbd6c5818d57829efe79e1cfe3a1cb0f29
micro-hawk <dasvikas0071@gmail.com>
2021-04-28 22:44:10 +0530

    installed jquery and navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,22 +1,49 @@
 import React from 'react';
 import "./Header.css";
-
+import $ from 'jquery';
 export default function Header(props) {
+
+    $(document).ready(function() {
+        $(".menu-icon").on("click", function() {
+              $("nav ul").toggleClass("showing");
+        });
+  });
+
+  // Scrolling Effect
+
+  $(window).on("scroll", function() {
+        if($(window).scrollTop()) {
+              $('nav').addClass('black');
+        }
+
+        else {
+              $('nav').removeClass('black');
+        }
+  })
 
 
     return(
         <section id="header">
             <div className="wrapper">
             <header>
-              <a href="#" className="logo">MicroHawk</a>
-              <ul>
-                  <li><a href="#">Home</a></li>
-                  <li><a href="#">About</a></li>
-                  <li><a href="#">Services</a></li>
-                  <li><a href="#">Contact Us</a></li>
-                  <li><a href="#">Portfolio</a></li>
-              </ul>
-          </header>
+            <nav>
+               <div class="menu-icon">
+                  <i class="fa fa-bars fa-2x"></i>
+               </div>
+               <div class="logo">
+                  MicroHawk
+               </div>
+               <div class="menu">
+                  <ul>
+                     <li><a href="#">Home</a></li>
+                     <li><a href="#">About</a></li>
+                     <li><a href="#">Blog</a></li>
+                     <li><a href="#">Contact</a></li>
+
+                  </ul>
+               </div>
+            </nav>
+         </header>
             </div>
 
 
